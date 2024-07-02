83a408b14c5f35ba4e4f95ed71f2d4ce60cb6120
Lazuardi Naufal <lazuardi.n@samsung.com>
2024-07-02 14:03:13 +0700

    add vault get secret

--- Vault.psm1
+++ Vault.psm1
@@ -1,3 +1,21 @@
+function Get-VaultSecret {
+    [OutputType([PSCustomObject])]
+    param (
+        [Alias("VaultUrl")] [ValidateNotNullOrWhiteSpace()] [string]$url,
+        [Alias("VaultPath")] [ValidateNotNullOrWhiteSpace()] [string]$path,
+        [Alias("AuthToken")] [ValidateNotNullOrWhiteSpace()] [string]$token,
+        [Alias("RenewLease")] [switch]$renew,
+        [Alias("OnErrorContinue")] [switch]$silent
+    )
+    $headers = @{ "X-Vault-Token" = $token }
+    try {
+        $result = Invoke-RestMethod -Uri "$($url)/v1/$($path)" -Method Get -Headers $headers
+        if ($renew) { Update-VaultLease $url $token | Out-Null }
+        return $result.data
+    }
+    catch { if ($silent) { return $null } else { throw } }
+}
+
 function Update-VaultLease {
     [OutputType([PSCustomObject])]
     param (
